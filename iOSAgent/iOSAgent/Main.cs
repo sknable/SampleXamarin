095ce0b0d8919001c6868f8f72ec572340e5df1b
Steve Knable <steve@steveknable.com>
2013-08-21 20:29:55 -0500

    Added Web Logic

--- iOSAgent/iOSAgent/Main.cs
+++ iOSAgent/iOSAgent/Main.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
+using SODA;
 
 namespace iOSAgent
 {
@@ -11,6 +12,13 @@ namespace iOSAgent
 		// This is the main entry point of the application.
 		static void Main (string[] args)
 		{
+
+            SODAClient _agent = new SODAClient();
+
+            _agent.LoginAgent("", "", "", "");
+
+            _agent.Run();
+
 			// if you want to use a different Application Delegate class from "AppDelegate"
 			// you can specify it here.
 			UIApplication.Main (args, null, "AppDelegate");
